f6cac62cf67c222d9252d172620f117c3c3d50a5
lucasgian <gianextensao@gmail.com>
2018-08-03 20:51:15 -0400

    #8 refactor: as funções, soundNotice e notifyMe, vieram para aqui

---
+++ public/javascripts/Notify.js
@@ -0,0 +1,48 @@
+class Notify {
+
+	// notificação com som
+
+	static soundNotice(url) {
+
+		const audio = new Audio();
+		audio.src = url;
+
+		const promise = audio.play();
+	
+		if (promise !== undefined) {
+
+			promise.then(_ => {
+
+				setTimeout(function() {
+					audio.pause();
+				}, 5000);
+
+			})
+			.catch(error => {
+
+				console.log(error);
+
+			});
+
+		}
+		
+	}
+
+	// chrome-notification
+	static notifyMe(title, container) {
+
+		if (Notification.permission !== "granted") {
+			Notification.requestPermission();
+		}
+		
+		else {
+	
+			const notification = new Notification(title, {
+				icon: 'https://pbs.twimg.com/profile_images/644212176065024000/9cC8iFCN_400x400.jpg',
+				body: container,
+			});
+	
+		}
+	
+	}
+}
