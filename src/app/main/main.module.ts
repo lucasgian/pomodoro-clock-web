7cffa9b7b31460dd0a1a66271532c2e7259d1196
gian.fonseca <gian.fonseca@saipos.com>
2020-03-02 10:16:28 -0300

    implemented main

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -3,6 +3,13 @@ import { CommonModule } from '@angular/common';
 import { MainRoutingModule } from './main-routing.module';
 import { ClockComponent } from './clock/clock.component';
 import { MainComponent } from './main.component';
+import { FormsModule } from '@angular/forms';
+import { CountdownModule } from 'ngx-countdown';
+import { CountdownComponent } from 'ngx-countdown';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -11,7 +18,15 @@ import { MainComponent } from './main.component';
   ],
   imports: [
     CommonModule,
-    MainRoutingModule
-  ]
+    MainRoutingModule,
+    FormsModule,
+    CountdownModule,
+    MatCardModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatIconModule
+  ],
+  exports: [],
+  providers: [CountdownComponent]
 })
 export class MainModule { }
