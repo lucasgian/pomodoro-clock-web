a4eed0cd4dc3df601478c0f026b0aabbf772351f
gian.fonseca <gian.fonseca@saipos.com>
2020-03-02 10:16:08 -0300

    added component clock

--- src/app/main/clock/clock.component.ts
+++ src/app/main/clock/clock.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, ViewChild, OnInit } from '@angular/core';
+import { CountdownComponent } from 'ngx-countdown';
 
 @Component({
   selector: 'app-clock',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClockComponent implements OnInit {
 
+  @Input() title: String
+  @Input() time: number = 0
+  @ViewChild('cd') private counter: CountdownComponent
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
+  subtractValue() {
+    this.time--
+  }
+
+  stop() {
+    this.counter.stop()
+  }
+
+  addValue() {
+    this.time++
+  }
 }
